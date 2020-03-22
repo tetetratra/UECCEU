c1f239e604c54f6606c69defb4e133c98c2827be
tetetratra <44367208+tetetratra@users.noreply.github.com>
2020-03-22 20:29:05 +0900

    jquery使えるようにした

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+// [Rails 6 でjQueryを使う - Qiita](https://qiita.com/hizukayudai/items/4eea87cb9356860eec62)
+// ここ見て書いた
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
 module.exports = environment
