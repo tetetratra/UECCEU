03699fefd25ae9baf76829f804b396a079657cae
daichi kondo <kurutoga.kingyo@gmail.com>
2020-03-23 03:37:27 +0900

    コメントの名前機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   def create
     params.permit! # アクセスできないのを許可
-    comment = Comment.new(text: params[:comment][:text], course_id: params[:course_id])
+    comment = Comment.new(text: params[:comment][:text], course_id: params[:course_id], contributor: params[:comment][:contributor])
     course = Course.find(params[:course_id])
     if comment.save
       redirect_to "/#{course.year}/#{course.url_name}/#{course.inner_index}#comment_form", notice: 'コメントしました'
     else
-      if params[:comment][:text].empty?
+      if params[:comment][:text].strip.empty?
         redirect_to "/#{course.year}/#{course.url_name}/#{course.inner_index}#comment_form", notice: '空文字のコメントは出来ません'
       else
         redirect_to "/#{course.year}/#{course.url_name}/#{course.inner_index}#comment_form", notice: 'コメント処理時にエラーが起きました'
