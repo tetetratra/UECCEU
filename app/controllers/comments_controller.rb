b775c81cbbb5816a50918470bd288abf247413b0
daichi kondo <kurutoga.kingyo@gmail.com>
2020-03-23 01:46:05 +0900

    コメントの表示などを改修

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,13 @@ class CommentsController < ApplicationController
     comment = Comment.new(text: params[:comment][:text], course_id: params[:course_id])
     course = Course.find(params[:course_id])
     if comment.save
-      redirect_to "/#{course.year}/#{course.url_name}#comment_form", notice: 'コメントしました'
+      redirect_to "/#{course.year}/#{course.url_name}/#{course.inner_index}#comment_form", notice: 'コメントしました'
     else
-      redirect_to "/#{course.year}/#{course.url_name}#comment_form", notice: 'コメント処理時にエラーが起きました'
+      if params[:comment][:text].empty?
+        redirect_to "/#{course.year}/#{course.url_name}/#{course.inner_index}#comment_form", notice: '空文字のコメントは出来ません'
+      else
+        redirect_to "/#{course.year}/#{course.url_name}/#{course.inner_index}#comment_form", notice: 'コメント処理時にエラーが起きました'
+      end
     end
   end
 end
