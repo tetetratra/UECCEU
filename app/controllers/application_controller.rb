f56016f237c397b33e979424c67f062ebb468ecb
tetetratra <44367208+tetetratra@users.noreply.github.com>
2020-03-22 10:32:48 +0900

    toppageを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  def top_page
+    render template: 'top_page', layout: 'application', content_type: 'text/html'
+  end
+
   def render_404
     render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
   end
