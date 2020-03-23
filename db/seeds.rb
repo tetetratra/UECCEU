5e41b129acdf8627fca977b1d2815aacd596121f
tetetratra <44367208+tetetratra@users.noreply.github.com>
2020-03-23 14:46:59 +0900

    inner_indexだけ付ける処理をseedに書いた

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require_relative '../lib/crawler/handler.rb'
+# require_relative '../lib/crawler/handler.rb'
 # クロールしてDBに書き込み
 
+require_relative '../lib/add_inner_index.rb'
