86737670c3457b62249e6c1d194bdd19b99f5332
daichi kondo <kurutoga.kingyo@gmail.com>
2020-03-23 13:51:25 +0900

    migrationファイルを正しく書き直し

--- db/migrate/20200323021437_initialize_database.rb
+++ db/migrate/20200323021437_initialize_database.rb
@@ -1,64 +1,53 @@
 class InitializeDatabase < ActiveRecord::Migration[6.0]
   def up
-    sql=<<SQL
--- Create syntax for TABLE 'courses'
-CREATE TABLE `courses` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `inner_index` int(11) DEFAULT NULL,
-  `course_title_japanese` text,
-  `course_title_english` text,
-  `code` text,
-  `academic_year` text,
-  `year_offerd` text,
-  `semester_offerd` text,
-  `faculty_offering_the_course` text,
-  `teaching_method` text,
-  `credits` text,
-  `category` text,
-  `cluster_and_department` text,
-  `lecturer` text,
-  `office` text,
-  `email` text,
-  `course_website` text,
-  `last_updated` text,
-  `update_status` text,
-  `topic_and_goals` text,
-  `prerequisites` text,
-  `recommended_prerequisites_and_preparation` text,
-  `course_textbooks_and_materials` text,
-  `course_outline_and_weekly_schedule` text,
-  `course_content_utilizing_practical_experience` text,
-  `preparation_and_review_outside_class` text,
-  `evaluation_and_grading` text,
-  `office_hours` text,
-  `message_for_students` text,
-  `other` text,
-  `keyword` text,
-  `year` int(11) DEFAULT NULL,
-  `url_name` text,
-  `created_at` datetime(6) NOT NULL,
-  `updated_at` datetime(6) NOT NULL,
-  `day_and_period` text,
-  `timetable_code` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-SQL
-    sql2=<<SQL
--- Create syntax for TABLE 'comments'
-CREATE TABLE `comments` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `text` text,
-  `contributor` text,
-  `created_at` datetime(6) NOT NULL,
-  `updated_at` datetime(6) NOT NULL,
-  `course_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `index_comments_on_course_id` (`course_id`),
-  CONSTRAINT `fk_rails_d0f578074a` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-SQL
-    ActiveRecord::Base.connection.execute(sql)
-    ActiveRecord::Base.connection.execute(sql2)
+    create_table :courses do |t|
+      t.integer :inner_index
+      t.integer :year
+      t.text :url_name
+
+      t.text :course_title_japanese
+      t.text :course_title_english
+      t.text :code
+      t.text :academic_year
+      t.text :year_offerd
+      t.text :semester_offerd
+      t.text :faculty_offering_the_course
+      t.text :teaching_method
+      t.text :credits
+      t.text :category
+      t.text :cluster_and_department
+      t.text :lecturer
+      t.text :office
+      t.text :email
+      t.text :course_website
+      t.text :last_updated
+      t.text :update_status
+      t.text :topic_and_goals
+      t.text :prerequisites
+      t.text :recommended_prerequisites_and_preparation
+      t.text :course_textbooks_and_materials
+      t.text :course_outline_and_weekly_schedule
+      t.text :course_content_utilizing_practical_experience
+      t.text :preparation_and_review_outside_class
+      t.text :evaluation_and_grading
+      t.text :office_hours
+      t.text :message_for_students
+      t.text :other
+      t.text :keyword
+      t.text :day_and_period
+      t.text :timetable_code
+
+      t.timestamps
+    end
+
+    create_table :comments do |t|
+      t.references :course, foreign_key: true
+
+      t.text :text
+      t.text :contributor
+
+      t.timestamps
+    end
   end
 
   def down
