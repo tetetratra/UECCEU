d0e4c6fdbd93ee81ec57dcf1e86f8f3313576293
tetetratra <44367208+tetetratra@users.noreply.github.com>
2020-03-28 21:18:50 +0900

    reactやめた Revert "react-railsが動くようにした"
    This reverts commit fddb4567281fc5d74bdf38d8260a1639f8079f26.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -159,7 +159,3 @@ $(
 
 
 
-// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
